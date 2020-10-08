0c953c7be44e5eec4fa2660bb34257a169479e35
Migy Yabut <jmaryabut@gmail.com>
2020-10-08 14:08:45 +0800

    Add files via upload

---
+++ AppDelegate.swift
@@ -0,0 +1,44 @@
+//
+//  AppDelegate.swift
+//  Project ARM
+//
+//  Created by Migy Yabut on 21/06/2018.
+//  Copyright © 2018 Migy Yabut. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+
+    }
+
+
+}
+
